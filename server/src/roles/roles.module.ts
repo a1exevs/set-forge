520456977be1dd6fa250a54be746aeaa7d5aeddf
Alexander Evstafiadi <aleksandrevstafiadi@gmail.com>
2026-08-04 21:28:13 +0300

    upd: build fix

--- server/src/roles/roles.module.ts
+++ server/src/roles/roles.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 
-import { AuthModule } from '@auth/auth.module';
 import { Role } from '@roles/roles.model';
 import { RolesService } from '@roles/roles.service';
 
 @Module({
   controllers: [],
   providers: [RolesService],
-  imports: [SequelizeModule.forFeature([Role]), AuthModule],
+  imports: [SequelizeModule.forFeature([Role])],
   exports: [RolesService],
 })
 export class RolesModule {}
